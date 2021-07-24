696c4b54de730e9eb8d23823525a43e6efd362b7
Rafał Misiarek <misiarek@gmail.com>
2021-07-24 21:42:53 +0200

    Add Dockerfile (#1)

---
+++ kafka-kraft/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+SUUID=$(./bin/kafka-storage.sh random-uuid);
+
+./bin/kafka-storage.sh format -t $SUUID -c ./config/kraft/server.properties;
+./bin/kafka-server-start.sh ./config/kraft/server.properties
